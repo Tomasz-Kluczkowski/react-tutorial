23f14383256adc7f123371cdffb2ed7d22669b29
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2018-10-02 22:38:09 +0100

    Working with list of components. - done.

--- src/App.js
+++ src/App.js
@@ -21,10 +21,15 @@ class App extends Component {
     nameChangedHandler = (event, id) => {
         const personIndex = this.state.persons.findIndex(p => {
             return p.id === id;
-        })
+        });
         const person = {
             ...this.state.persons[personIndex]};
 
+        person.name = event.target.value;
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState({persons: persons})
     };
 
     togglePersonsHandler = () => {
